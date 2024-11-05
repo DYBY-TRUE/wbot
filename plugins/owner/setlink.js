8f4c2f946f70672cf7f025896fc1e87e0627c89f
dyby-tech1 <herokutruem@gmail.com>
2024-11-05 07:24:36 -0500

    Update setlink.js

--- plugins/owner/setlink.js
+++ plugins/owner/setlink.js
@@ -13,9 +13,9 @@ exports.run = {
          let setting = global.db.setting
          if (!text) return client.reply(m.chat, Func.example(isPrefix, command, global.db.setting.link), m)
          const isUrl = Func.isUrl(text)
-         if (!isUrl) return client.reply(m.chat, Func.texted('bold', `🚩 URL is invalid.`), m)
+         if (!isUrl) return client.reply(m.chat, Func.texted('bold', `✨ URL is invalid.`), m)
          setting.link = text
-         client.reply(m.chat, Func.texted('bold', `🚩 Link successfully set.`), m)
+         client.reply(m.chat, Func.texted('bold', `✨ Link successfully set.`), m)
       } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }

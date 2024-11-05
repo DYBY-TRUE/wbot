2f8071d4816e52cee65988277b31301dcba7503b
dyby-tech1 <herokutruem@gmail.com>
2024-11-05 02:37:21 -0500

    Update cmdstic_list.js

--- plugins/owner/cmdstic_list.js
+++ plugins/owner/cmdstic_list.js
@@ -8,7 +8,7 @@ exports.run = {
       Func
    }) => {
       let cmdS = Object.keys(global.db.sticker)
-      if (cmdS.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 No sticker commands.`), m)
+      if (cmdS.length == 0) return client.reply(m.chat, Func.texted('bold', `✨ No sticker commands.`), m)
       let teks = `乂  *C M D - L I S T*\n\n`
       for (let i = 0; i < cmdS.length; i++) {
          teks += Func.texted('bold', (i + 1) + '.') + ' ' + cmdS[i] + '\n'

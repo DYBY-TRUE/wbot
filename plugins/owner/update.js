4d98946221a089ed580918e5cec0aa6808f777e1
dyby-tech1 <herokutruem@gmail.com>
2024-11-05 02:28:14 -0500

    Update update.js

--- plugins/owner/update.js
+++ plugins/owner/update.js
@@ -9,12 +9,12 @@ exports.run = {
       try {
          var stdout = execSync('git pull')
          var output = stdout.toString()
-         if (output.match(new RegExp('Already up to date', 'g'))) return client.reply(m.chat, Func.texted('bold', `🚩 ${output.trim()}`), m)
+         if (output.match(new RegExp('Already up to date', 'g'))) return client.reply(m.chat, Func.texted('bold', `✨ ${output.trim()}`), m)
          if (output.match(/stash/g)) {
             var stdout = execSync('git stash && git pull')
             var output = stdout.toString()
-            client.reply(m.chat, `🚩 ${output.trim()}`, m).then(async () => process.send('reset'))
-         } else return client.reply(m.chat, `🚩 ${output.trim()}`, m).then(async () => process.send('reset'))
+            client.reply(m.chat, `✨ ${output.trim()}`, m).then(async () => process.send('reset'))
+         } else return client.reply(m.chat, `✨ ${output.trim()}`, m).then(async () => process.send('reset'))
       } catch (e) {
          return client.reply(m.chat, Func.jsonFormat(e), m)
       }
